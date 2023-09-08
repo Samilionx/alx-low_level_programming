b25b274b31fcf3ee9761bfb1f26f56220039bb45
Samilionx <samilion@gmail.com>
2023-09-07 21:58:53 -0700

    coding

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints a line usinf printf function 
+ * return 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
