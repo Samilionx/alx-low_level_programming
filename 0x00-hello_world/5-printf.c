770f70c5f15fa363a3ee389e5c745c427aa6e4fb
Samilionx <samilion@gmail.com>
2023-09-08 18:17:38 -0700

    compiling

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A c program that prints a line usinf printf function 
+ * main - A c program that prints a line usinf printf function
  * return 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,")
 return (0);
 }
