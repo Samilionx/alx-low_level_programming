03ce80f829fdeb48b8360811b268a8a6ec4dbe23
Samilionx <samilion@gmail.com>
2023-09-19 16:49:02 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+*a function that printsn
+*elements of an array of integers, followed by a new line.
+*/
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < (n - 1); i++)
+{
+printf("%d, ", a[i]);
+}
+if (i == (n -1))
+{
+printf("%d", a[n - 1]);
+}
+printf('\n');
+}
