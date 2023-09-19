ad9c6762d4860cc23747694486d047e58c0bb39d
Samilionx <samilion@gmail.com>
2023-09-19 15:43:12 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,8 +6,11 @@
  */
 int _strlen(char *s)
 {
-int j;
-int count = 0;
-for (j = 0; s[j] != '\0'; j++)
-count++;
-return (count);
+int longi = 0;
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+return (longi);
+}
