0d9b82ba2501dd6bc4b8d173f4ca23420c382d9d
Samilionx <samilion@gmail.com>
2023-10-24 16:16:27 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
-stint_safe - frees a linked list
+ *stint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
  *
  * Return: number of elements in the freed list
